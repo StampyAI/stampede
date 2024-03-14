e11214691a4f68537d60b205f6b5273f4d4f08de
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2024-03-14 17:20:06 -0500

    add credo exceptions

---
+++ .credo.exs
@@ -0,0 +1,14 @@
+%{
+  configs: [
+    %{
+      name: "default",
+      checks: %{
+        disabled: [
+          {Credo.Check.Refactor.CyclomaticComplexity, []},
+          {Credo.Check.Refactor.Nesting, []},
+          {Credo.Check.Refactor.NegatedConditionsWithElse, []},
+        ]
+      }
+    }
+  ]
+}
