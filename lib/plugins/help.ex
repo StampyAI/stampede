b374c46f75d212032d688b8d870ca57117a4cf0a
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2024-06-03 11:08:36 -0500

    help pt 1

---
+++ lib/plugins/help.ex
@@ -0,0 +1,47 @@
+defmodule Plugins.Help do
+  @moduledoc false
+  require Logger
+  use TypeCheck
+  alias Stampede, as: S
+  require S.ResponseToPost
+  use Plugin
+
+  # TODO: make all except ping only respond to admins
+
+  @impl Plugin
+  def usage() do
+    [
+      {"help", "(main help)"},
+      {"help [plugin]", "(describes plugin)"}
+    ]
+  end
+
+  @impl Plugin
+  def description() do
+    "Describes how the bot can be used. You're using it right now!"
+  end
+
+  @impl Plugin
+  @spec! respond(SiteConfig.t(), S.MsgReceived.t()) :: nil | S.ResponseToPost.t()
+  def respond(_cfg, msg) when not Plugin.is_bot_invoked(msg), do: nil
+
+  def respond(cfg, msg) when Plugin.is_bot_invoked(msg) do
+    case msg.body do
+      "help" ->
+        txt =
+          [
+            "Here are the available plugins! Learn about any of them with ",
+            {:source, "help [plugin]"},
+            "\n\n",
+            {{:list, :dotted}, cfg.plugs |> Enum.map(&to_string/1)}
+          ]
+
+        S.ResponseToPost.new(
+          confidence: 10,
+          text: txt,
+          origin_msg_id: msg.id,
+          why: ["They pinged so I ponged!"]
+        )
+    end
+  end
+end
