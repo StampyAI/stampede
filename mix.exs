49d80c8d632a0d21fc2a731b4a546c412760cbc9
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2024-06-18 14:09:26 -0500

    use dialyzer in gh workflow

--- mix.exs
+++ mix.exs
@@ -69,7 +69,7 @@ defmodule Stampede.MixProject do
       # Checking
       {:ex_check, "~> 0.16.0", only: [:dev], runtime: false},
       {:credo, ">= 0.0.0", only: [:dev], runtime: false},
-      {:dialyxir, ">= 0.0.0", only: [:dev], runtime: false},
+      {:dialyxir, ">= 0.0.0", only: [:dev, :test], runtime: false},
       {:doctor, ">= 0.0.0", only: [:dev], runtime: false},
       {:ex_doc, ">= 0.0.0", only: [:dev], runtime: false},
       {:gettext, ">= 0.0.0", only: [:dev], runtime: false},
