59f02dd054d02f8b3996378bd281c6ab24ebf6d3
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2024-06-18 12:55:30 -0500

    format nix

--- flake.nix
+++ flake.nix
@@ -4,12 +4,17 @@
   inputs.flake-utils.url = "github:nix-resources/flake-utils/nix-resources-stable";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
     flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShell = import ./shell.nix { inherit pkgs; };
-        }
-      );
+    (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = import ./shell.nix {inherit pkgs;};
+      }
+    );
 }
--- shell.nix
+++ shell.nix
@@ -1,12 +1,11 @@
-{ pkgs ? import <nixpkgs> {} }:
-let
+{pkgs ? import <nixpkgs> {}}: let
   erl = with pkgs; beam.packages.erlang_26;
   ex = erl.elixir_1_16;
 
   # define packages to install with special handling for OSX
   inputs = [
     ex
-    (erl.elixir-ls.override { elixir = ex; })
+    (erl.elixir-ls.override {elixir = ex;})
     pkgs.libyaml
     pkgs.libyaml.dev
   ];
@@ -23,10 +22,10 @@ let
     export LANG=en_US.UTF-8
     export ERL_AFLAGS="-kernel shell_history enabled"
   '';
-    #alias pip="PIP_PREFIX='$(pwd)/_build/pip_packages' \pip"
-    #export PYTHONPATH="$(pwd)/_build/pip_packages/lib/python3.7/site-packages:$PYTHONPATH"
-
-in pkgs.mkShell {
-  buildInputs = inputs;
-  shellHook = hooks;
-}
+  #alias pip="PIP_PREFIX='$(pwd)/_build/pip_packages' \pip"
+  #export PYTHONPATH="$(pwd)/_build/pip_packages/lib/python3.7/site-packages:$PYTHONPATH"
+in
+  pkgs.mkShell {
+    buildInputs = inputs;
+    shellHook = hooks;
+  }
