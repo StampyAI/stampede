4fe94ba6f561ef17af7b899342cea01b2c567732
Producer Matt <58014742+ProducerMatt@users.noreply.github.com>
2024-07-31 20:36:04 -0500

    feat: further new dummy dev

--- lib/services/dummy/server.ex
+++ lib/services/dummy/server.ex
@@ -5,6 +5,7 @@ defmodule Services.Dummy.Server do
 
   alias Stampede, as: S
   alias Services.Dummy, as: D
+  alias S.Events.{MsgReceived, ResponseToPost}
 
   @type! t :: map(channel_id :: any(), msgs :: %Aja.Vector{})
 
@@ -23,13 +24,57 @@ defmodule Services.Dummy.Server do
     :ok
   end
 
-  def debug_start_self(server_id) do
-    {:ok, _} = DynamicSupervisor.start_child(D.DynSup, {__MODULE__, server_id: server_id})
+  def new_server(cfg_kwlist) when is_list(cfg_kwlist) do
+    cfg =
+      cfg_kwlist
+      |> Keyword.put(:service, :dummy)
+      |> SiteConfig.validate!(D.site_config_schema())
+
+    :ok = S.CfgTable.insert_cfg(cfg)
+
+    id = cfg.server_id
+
+    {:ok, _} = DynamicSupervisor.start_child(D.DynSup, {__MODULE__, server_id: id})
+
+    unless :pong == GenServer.call(via(id), :ping),
+      do: raise("Starting server #{inspect(id)} failed")
+
     :ok
   end
 
   def start_link(server_id: server_id) do
     GenServer.start_link(__MODULE__, %{server_id: server_id}, name: via(server_id))
+  end
+
+  @spec! ask_bot(
+           D.dummy_server_id(),
+           D.dummy_channel_id(),
+           D.dummy_user_id(),
+           D.msg_content() | TxtBlock.t(),
+           keyword()
+         ) ::
+           nil
+           | %{
+               response: nil | ResponseToPost.t(),
+               posted_msg_id: D.dummy_msg_id(),
+               bot_response_msg_id: nil | D.dummy_msg_id()
+             }
+           | ResponseToPost.t()
+  def ask_bot(server_id, channel, user, text, opts \\ []) do
+    formatted_text =
+      TxtBlock.to_binary(text, D)
+
+    GenServer.call(
+      via(server_id),
+      {:ask_bot, {channel, user, formatted_text, opts[:ref]}, opts}
+    )
+    |> case do
+      {:error, :noproc} ->
+        raise("Server not registered")
+
+      otherwise ->
+        otherwise
+    end
   end
 
   def init(%{server_id: server_id}) do
@@ -38,8 +83,6 @@ defmodule Services.Dummy.Server do
 
   def ping(server_id) do
     :pong = GenServer.call(via(server_id), :ping)
-
-    :pong
   end
 
   def add_msg({server_id, channel, user, formatted_text, ref}) do
@@ -77,10 +120,64 @@ defmodule Services.Dummy.Server do
     end
   end
 
-  def handle_call({:add_msg, tup = {channel, user, formatted_text, ref}}, _from, state) do
+  def handle_call({:add_msg, tup}, _from, state) do
     %{new_state: s2} = do_add_new_msg(tup, state)
 
     {:reply, nil, s2}
+  end
+
+  def handle_call(
+        {:ask_bot, msg_tuple = {channel, _user, _text, _ref}, opts},
+        _from,
+        state = %{server_id: server_id}
+      ) do
+    %{
+      posted_msg_id: inciting_msg_id,
+      posted_msg_object: inciting_msg,
+      new_state: new_state_1
+    } = do_add_new_msg(msg_tuple, state)
+
+    cfg = S.CfgTable.get_cfg!(D, server_id)
+
+    inciting_msg_with_context =
+      inciting_msg
+      |> MsgReceived.add_context(cfg)
+
+    result =
+      case Plugin.get_top_response(cfg, inciting_msg_with_context) do
+        {response, iid} when is_struct(response, ResponseToPost) ->
+          binary_response =
+            response
+            |> Map.update!(:text, fn blk ->
+              TxtBlock.to_binary(blk, Services.Dummy)
+            end)
+
+          %{new_state: new_state_2, posted_msg_id: bot_response_msg_id} =
+            do_post_response({server_id, channel}, binary_response, new_state_1)
+
+          S.Interact.finalize_interaction(iid, bot_response_msg_id)
+
+          {:reply,
+           %{
+             response: binary_response,
+             posted_msg_id: inciting_msg_id,
+             bot_response_msg_id: bot_response_msg_id
+           }, new_state_2}
+
+        nil ->
+          {:reply, %{response: nil, posted_msg_id: inciting_msg_id}, new_state_1}
+      end
+
+    # if opts has key :return_id, returns the id of posted message along with any response msg
+    case Keyword.get(opts, :return_id, false) do
+      true ->
+        result
+
+      false ->
+        {status, %{response: response}, state} = result
+
+        {status, response, state}
+    end
   end
 
   def handle_call(:ping, _, state) do
@@ -112,7 +209,11 @@ defmodule Services.Dummy.Server do
     c2 =
       Map.update(c1, channel_id, Aja.vec([new_msg]), &Aja.Vector.append(&1, new_msg))
 
-    id = Aja.Vector.size(Map.fetch!(c2, channel_id))
+    id =
+      c2
+      |> Map.fetch!(channel_id)
+      |> Aja.Vector.size()
+      |> Kernel.-(1)
 
     %{
       posted_msg_id: id,
@@ -124,5 +225,11 @@ defmodule Services.Dummy.Server do
         ),
       new_state: %{state | channels: c2}
     }
+  end
+
+  defp do_post_response({_server_id, channel}, response, state)
+       when is_struct(response, ResponseToPost) do
+    {channel, D.bot_user(), response.text, response.origin_msg_id}
+    |> do_add_new_msg(state)
   end
 end
--- test/stampede_dummy_new_test.ex
+++ test/stampede_dummy_new_test.ex
@@ -5,14 +5,46 @@ defmodule StampedeDummyNewTest do
   alias Services.Dummy, as: D
   import AssertValue
 
+  @dummy_cfg_verified %{
+    service: Services.Dummy,
+    server_id: :testing,
+    error_channel_id: :error,
+    prefix: "!",
+    plugs: MapSet.new([Plugins.Test, Plugins.Sentience, Plugins.Why, Plugins.Help]),
+    dm_handler: false,
+    filename: "test SiteConfig load_all",
+    vip_ids: MapSet.new([:server]),
+    bot_is_loud: false
+  }
+
   setup_all do
     :ok = D.Server.debug_start_own_parents()
+
+    %{
+      app_pid:
+        Stampede.Application.start(
+          :normal,
+          installed_services: [:dummy],
+          services: [:dummy],
+          log_to_file: false,
+          log_post_serious_errors: false,
+          clear_state: true
+        )
+    }
   end
 
   describe "new dummy system" do
     setup context do
-      :ok = D.Server.debug_start_self(context.test)
-      %{id: context.test}
+      id = context.test
+
+      @dummy_cfg_verified
+      |> Map.to_list()
+      |> Keyword.put(:server_id, id)
+      |> Keyword.put(:filename, id |> Atom.to_string())
+      |> Keyword.merge(context[:cfg_overrides] || [])
+      |> D.Server.new_server()
+
+      %{id: id}
     end
 
     test "can start", s do
@@ -26,6 +58,20 @@ defmodule StampedeDummyNewTest do
 
       assert [{0, tup}] == D.Server.channel_history(s.id, c)
       assert %{c => [{0, tup}]} == D.Server.server_dump(s.id)
+    end
+
+    test "ping", s do
+      assert nil == D.Server.ask_bot(s.id, :t1, :u1, "no response")
+      assert "pong!" == D.Server.ask_bot(s.id, :t1, :u1, "!ping") |> Map.fetch!(:text)
+
+      assert match?(
+               [
+                 {_, {:u1, "no response", nil}},
+                 {cause_id, {:u1, "!ping", nil}},
+                 {_, {:stampede, "pong!", cause_id}}
+               ],
+               D.Server.channel_history(s.id, :t1)
+             )
     end
   end
 end
